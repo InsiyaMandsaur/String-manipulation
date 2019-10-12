7ef5afeaee2ba3beb531ca7510bcd4a3a40ab3e5
InsiyaMandsaur <55918162+InsiyaMandsaur@users.noreply.github.com>
2019-10-12 22:07:08 +0500

    amending fist word and second word
    string manipulation where you enter two strings and you get an amended string with the fist word from the first string and second word from the second string

---
+++ joining first word and second word.vb
@@ -0,0 +1,30 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim string1, string2, firstword, secondword As String
+        Dim spacepos As Integer
+
+        string1 = ""
+        string2 = ""
+        firstword = ""
+        secondword = ""
+        spacepos = 0
+
+        Console.WriteLine("Enter first string: ")
+        string1 = Console.ReadLine
+
+        Console.WriteLine("Enter second string: ")
+        string2 = Console.ReadLine
+
+        spacepos = InStr(string1, " ")
+        firstword = Left(string1, spacepos - 1)
+
+        spacepos = InStr(string2, " ")
+        secondword = Right(string2, Len(string2) - spacepos)
+
+        Console.WriteLine("Final string is: " & firstword & " " & secondword)
+
+        Console.ReadKey()
+    End Sub
+
+End Module
