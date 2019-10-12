d08e26f303e08588926c48de189c3e19f1f6d4eb
InsiyaMandsaur <55918162+InsiyaMandsaur@users.noreply.github.com>
2019-10-12 22:24:54 +0500

    finds the middle word
    where you enter a string of 3 words and it gives you its middle word

---
+++ middleword.vb
@@ -0,0 +1,24 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim string1, middleword As String
+        Dim spacepos1, spacepos2 As Integer
+
+        string1 = ""
+        middleword = ""
+        spacepos1 = 0
+        spacepos2 = 0
+
+        Console.WriteLine("Enter name that consists of 3 words: ")
+        string1 = Console.ReadLine
+
+        spacepos1 = InStr(string1, " ")
+        spacepos2 = InStr(spacepos1 + 1, string1, " ")
+        middleword = Mid(string1, spacepos1 + 1, spacepos2 - spacepos1 - 1)
+
+        Console.WriteLine(middleword)
+        Console.ReadKey()
+
+    End Sub
+
+End Module
