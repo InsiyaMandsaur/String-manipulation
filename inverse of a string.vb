13e767f20f3e78475897448bf349fa712b98357e
InsiyaMandsaur <55918162+InsiyaMandsaur@users.noreply.github.com>
2019-10-14 21:08:46 +0500

    inverse of a string
    it inverses a string entered

---
+++ inverse of a string.vb
@@ -0,0 +1,23 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim string1, string2, nextchar As String
+        Dim counter As Integer
+        nextchar = ""
+        string1 = ""
+        string2 = ""
+        counter = 0
+
+        Console.WriteLine("Enter the string: ")
+        string1 = Console.ReadLine
+
+        For counter = Len(string1) To 1 Step -1
+            nextchar = Mid(string1, counter, 1)
+            string2 = string2 & nextchar
+
+        Next
+        Console.WriteLine(string2)
+        Console.ReadKey()
+    End Sub
+
+End Module
