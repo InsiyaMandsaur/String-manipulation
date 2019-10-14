83570ec9f65db2e4ea448869025f17c25d7359bd
InsiyaMandsaur <55918162+InsiyaMandsaur@users.noreply.github.com>
2019-10-14 21:20:34 +0500

    is anagram?
    shows whether the entered strings are anagram or not.

---
+++ is anagram.vb
@@ -0,0 +1,35 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim string1, string2, nextchar As String
+        Dim counter As Integer
+        Dim isanagram As Boolean
+
+        string1 = ""
+        string2 = ""
+        nextchar = ""
+        counter = 0
+        isanagram = True
+
+
+        Console.WriteLine("Enter the string: ")
+        string1 = Console.ReadLine
+        Console.WriteLine("Enter the anagram: ")
+        string2 = Console.ReadLine
+
+        For counter = 1 To Len(string1)
+            nextchar = Mid(string1, counter, 1)
+            If InStr(string2, nextchar) = 0 Then
+                isanagram = False
+            End If
+        Next
+        If isanagram = False Then
+            Console.WriteLine("this in not anagram")
+            Console.ReadKey()
+        End If
+
+        Console.WriteLine("this is anagram")
+        Console.ReadKey()
+    End Sub
+
+End Module
