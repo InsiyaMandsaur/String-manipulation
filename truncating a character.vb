b499de0a311592b153978d2edebce0f7b0bc0790
InsiyaMandsaur <55918162+InsiyaMandsaur@users.noreply.github.com>
2019-10-14 21:00:50 +0500

    truncating a character
    it truncates a character you want from the string you enter

---
+++ truncating a character.vb
@@ -0,0 +1,29 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim string1, string3, mychar, nextchar As String
+        Dim counter As Integer
+
+        nextchar = ""
+        string1 = ""
+        string3 = ""
+        mychar = ""
+        counter = 0
+
+        Console.WriteLine("Enter your string: ")
+        string1 = Console.ReadLine
+
+        Console.Write("Enter the character you want to truncate: ")
+        mychar = Console.ReadLine
+
+        For counter = 1 To Len(string1)
+            nextchar = Mid(string1, counter, 1)
+            If nextchar <> mychar Then
+                string3 = string3 & nextchar
+            End If
+        Next
+        Console.WriteLine("your final string is: " & string3)
+        Console.ReadKey()
+    End Sub
+
+End Module
